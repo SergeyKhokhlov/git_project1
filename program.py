2f52b21d4cfaaa379cff0eaf658da63ac060e172
SergeyKhokhlov <46843695+SergeyKhokhlov@users.noreply.github.com>
2019-11-13 16:36:12 +0300

    Update program.py
    GitHub changed

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('My first git project')
     print('Add I change it every day')
     print('New commit from PyCharm')
+    print('New commit from Github')
 
 
 if __name__ == '__main__':
